7de99352a566675719461ecf27306f07192cc18a
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2022-07-27 19:55:04 -0300

    Altera texto de about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,11 +10,12 @@ export default function About() {
       <article id="img-container">
         <img id="img-perfil" src={ perfilImage } alt="Imagem de perfil" height="350" />
         <section>
-          <p className="about">Olá, me chamo</p>
           <p className="about" id="my-name">Gabriel Gomes Rodrigues</p>
-          <p className="about">Tenho 22 anos, sou desenvolvedor Front-End, moro em Uberlândia - MG e
-            atualmente estou me formando em Engenharia Ambiental, e estudando
-            Desenvolvimento Web na Trybe para me tornar um desenvolvedor Full Stack.
+          <p className="about">22 anos</p>
+          <p className="about">Uberlândia/MG</p>
+          <p className="about">Sou desenvolvedor Front-End e estudo Desenvolvimento Web pela Trybe 
+            com objetivo de me tornar um desenvolvedor Full Stack. Também sou discente de último 
+            período em Engenharia Ambiental pela Universidade Federal de Uberlândia.
           </p>
         </section>
       </article>
