5c99ba38cc5ef1778ff6afa8b40969da02313f6d
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2022-08-10 18:42:46 -0300

    Corrige nome do componente

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Description from './pages/About';
+import About from './pages/About';
 import Projects from './pages/Projects';
 import Skills from './pages/Skills';
 import Warning from './pages/Warning';
@@ -8,7 +8,7 @@ import Warning from './pages/Warning';
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Description } />
+      <Route exact path="/" component={ About } />
       <Route path="/projects" component={ Projects } />
       <Route path="/skills" component={ Skills } />
       <Route path="/warning" component={ Warning } />
