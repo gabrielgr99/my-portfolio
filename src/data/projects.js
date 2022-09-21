b8232ab7be4c1c47222e731abb19038e3bd06e86
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2022-09-21 20:50:46 -0300

    Corrige responsividade da pagina de projetos, atualiza projetos e organiza pasta de imagens

---
+++ src/data/projects.js
@@ -0,0 +1,77 @@
+import pixelArtLogo from "../images/projects/pixel-art-logo.svg";
+import todoListLogo from "../images/projects/todo-list-logo.svg";
+import jsUnitTestsLogo from "../images/projects/javascript-unit-tests-logo.png"
+import rtlLogo from "../images/projects/rtl-logo.png";
+import dockerTodoListLogo from "../images/projects/docker-todo-list-logo.png";
+import allForOneLogo from "../images/projects/all-for-one-logo.png";
+import oneForAllLogo from "../images/projects/one-for-all-logo.png";
+import talkerManagerLogo from "../images/projects/talker-manager-logo.png";
+import storeManagerLogo from "../images/projects/store-manager-logo.png";
+
+const projects = [
+  {
+    name: "Pixel Art",
+    href: "https://pixels-art-project.vercel.app/",
+    src: pixelArtLogo,
+    alt: "pixel-art logo",
+    technologies: [ 'HTML', 'CSS', 'JavaScript' ]
+  },
+  {
+    name: "To Do List",
+    href: "https://to-do-list-beta-wheat.vercel.app/",
+    src: todoListLogo,
+    alt: "todo-list logo",
+    technologies: [ 'HTML', 'CSS', 'JavaScript' ]
+  },
+  {
+    name: "JavaScript Unit Tests",
+    href: "https://github.com/gabrielgr99/Project-JavaScript-Unit-Tests",
+    src: jsUnitTestsLogo,
+    alt: "js-unit-tests logo",
+    technologies: [ 'Jest' ]
+  },
+  {
+    name: "RTL",
+    href: "https://github.com/gabrielgr99/Project-React-Testing-Library",
+    src: rtlLogo,
+    alt: "react-testing-library logo",
+    technologies: [ 'React Testing Library', 'Jest' ]
+  },
+  {
+    name: "Docker To Do List",
+    href: "https://github.com/gabrielgr99/Project-Docker-Todo-List",
+    src: dockerTodoListLogo,
+    alt: "docker-todo-list logo",
+    technologies: [ 'Docker' ]
+  },
+  {
+    name: "All For One",
+    href: "https://github.com/gabrielgr99/Project-Mysql-All-For-One",
+    src: allForOneLogo,
+    alt: "all-for-one logo",
+    technologies: [ 'MySQL' ]
+  },
+  {
+    name: "One For All",
+    href: "https://github.com/gabrielgr99/Project-Mysql-One-For-All",
+    src: oneForAllLogo,
+    alt: "one-for-all logo",
+    technologies: [ 'MySQL', 'Normalização' ]
+  },
+  {
+    name: "Talker Manager",
+    href: "https://github.com/gabrielgr99/Project-Talker-Manager",
+    src: talkerManagerLogo,
+    alt: "talker-manager logo",
+    technologies: [ 'JavaScript', 'API' ]
+  },
+  {
+    name: "Store Manager",
+    href: "https://github.com/gabrielgr99/Project-Store-Manager",
+    src: storeManagerLogo,
+    alt: "store-manager logo",
+    technologies: [ 'JavaScript', 'API RESTful MSC', 'MySQL', 'Testes (Chai, Sinon, Sinon-Chai)' ]
+  }
+];
+
+export default projects;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,10 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import pixelArt from "../images/pixel-art-icon.svg";
-import toDoList from "../images/todo-list-icon.svg";
 import "./Projects.css";
 import "./ProjectsResponsive.css";
 import AppContext from "../context/AppContext";
 import { useContext } from "react";
+import projects from "../data/projects";
 
 export default function Projects() {
   const mode = useContext(AppContext);
@@ -14,28 +13,27 @@ export default function Projects() {
     <main>
       <Header />
       <article id="project-list">
-        <section className="project-container">
-          <a className={`project-item project-item-${ mode }`} href="https://pixels-art-project.vercel.app/" target="_blanked">
-            <section>
-              <h4>Pixel Art</h4>
-              <img src={ pixelArt } alt="Logo pixel art" width="150px" />
-              <p>HTML</p>
-              <p>CSS</p>
-              <p>JavaScript</p>
-            </section>
-          </a>
-        </section>
-        <section className="project-container">
-          <a className={`project-item project-item-${ mode }`} href="https://to-do-list-beta-wheat.vercel.app/" target="_blanked">
-            <section>
-              <h4>To-Do List</h4>
-              <img src={ toDoList } alt="Logo to do list" width="150px" />
-              <p>HTML</p>
-              <p>CSS</p>
-              <p>JavaScript</p>
-            </section>
-          </a>
-        </section>
+
+        {
+          projects.map((project) => {
+            return (
+              <section className="project-container">
+                <a className={`project-item project-item-${ mode }`} href={ project.href } target="_blanked">
+                  <section>
+                    <h4>{ project.name }</h4>
+                    <img src={ project.src } alt={ project.alt } width="150px" />
+                    <div>
+                      {
+                        project.technologies.map((technologie) => <p>{ technologie }</p>)
+                      }
+                    </div>
+                  </section>
+                </a>
+              </section>
+            )
+          })
+        }
+
       </article>
       <Footer value='footer-content-projects' />
     </main>
