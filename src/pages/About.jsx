ef3336fd617a922584ece68712ab75ba3be289e3
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2023-02-07 12:20:02 -0300

    feat: atualiza texto da página sobre

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,9 +18,10 @@ export default function About() {
           <p className={`my-name my-name-${ mode }`}>Gabriel Gomes Rodrigues</p>
           <p className="about">22 anos</p>
           <p className="about">Uberlândia/MG</p>
-          <p className="about">Sou desenvolvedor Front-End e estudo Desenvolvimento Web pela Trybe 
-            com objetivo de me tornar um desenvolvedor Full Stack. Também sou discente de último 
-            período em Engenharia Ambiental pela Universidade Federal de Uberlândia.
+          <p className="about">Sou desenvolvedor Full-Stack e formado em Engenharia Ambiental 
+            pela Universidade Federal de Uberlândia. Busco me tornar reconhecido pelo meu trabalho,
+            e para isso pretendo melhorar cada vez mais a qualidade dos meus códigos, deixando eles 
+            mais limpos e de fácil interpretação.
           </p>
         </section>
       </article>
